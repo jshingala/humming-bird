5c2ea26e33f1921bc7d0e34079d7ab33c5cb6902
Jenil Shingala <160362895+jshingala@users.noreply.github.com>
2025-01-30 13:23:52 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.InBackground)
+basic.forever(function () {
+	
+})
