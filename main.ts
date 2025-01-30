16431333d7a4abdd83d58180c1643568d1f519d8
Jenil Shingala <160362895+jshingala@users.noreply.github.com>
2025-01-30 13:29:26 -0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,6 @@ input.onButtonPressed(Button.A, function () {
 input.onGesture(Gesture.Shake, function () {
 	
 })
-music.play(music.stringPlayable("- - - - - - - - ", 120), music.PlaybackMode.InBackground)
 basic.forever(function () {
 	
 })
